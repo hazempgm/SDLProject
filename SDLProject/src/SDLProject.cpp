653b8eb3ad0b198937a6463c8ac556b045a3ad68
assassinidiot <assassinidiot@outlook.com>
2021-05-03 15:43:57 +0200

    Add project files.

---
+++ SDLProject/src/SDLProject.cpp
@@ -0,0 +1,12 @@
+﻿// SDLProject.cpp : Defines the entry point for the application.
+//
+
+#include "../include/SDLProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
